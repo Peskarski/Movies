9ad06d9c667955df5847c132565b5b2565c69410
Peskarsky <v.peskarskij@gmail.com>
2021-06-24 15:28:08 +0300

    set up react router

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -3,16 +3,21 @@ import { Dashboard } from './components/Dashboard';
 import { StyledContainer } from './styles';
 import './i18n/config';
 import { useTranslation } from 'react-i18next';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 const App = () => {
   const { t } = useTranslation();
 
   return (
-    <StyledContainer maxWidth={false}>
-      <h1>{t('title')}</h1>
-      <NavBar />
-      <Dashboard />
-    </StyledContainer>
+    <Router>
+      <StyledContainer maxWidth={false}>
+        <h1>{t('title')}</h1>
+        <NavBar />
+        <Switch>
+          <Route path="/dashboard" exact component={Dashboard} />
+        </Switch>
+      </StyledContainer>
+    </Router>
   );
 };
 
--- my-app/src/components/NavBar/NavBar.tsx
+++ my-app/src/components/NavBar/NavBar.tsx
@@ -4,6 +4,7 @@ import ButtonGroup from '@material-ui/core/ButtonGroup';
 import Tabs from '@material-ui/core/Tabs';
 import i18n from 'i18next';
 import { useTranslation } from 'react-i18next';
+import { useHistory } from 'react-router-dom';
 
 type language = {
   name: string;
@@ -18,12 +19,18 @@ const languages: language[] = [
 
 export const NavBar: React.FC = () => {
   const { t } = useTranslation();
+  const history = useHistory();
+
   return (
     <StyledContainer>
       <StyledAppBar position="static">
         <Tabs aria-label="simple tabs example" centered variant="fullWidth">
           {navTabs.map((tab) => (
-            <StyledTab label={t(tab)} key={String(Math.random())} />
+            <StyledTab
+              label={t(tab)}
+              key={String(Math.random())}
+              onClick={() => history.push(`/${t(tab, { lng: 'en' })}`)}
+            />
           ))}
         </Tabs>
       </StyledAppBar>
