a8aabefd65d11baeec3c9e1ce6f97ccb2faf3d21
Peskarsky <v.peskarskij@gmail.com>
2021-06-29 14:34:41 +0300

    rebased from master

--- my-app/src/API/index.ts
+++ my-app/src/API/index.ts
@@ -14,4 +14,8 @@ export const POPULAR_REQUEST_WORD = 'popular';
 
 //genres
 export const getGenresUrl = (lng: string) =>
-  `https://api.themoviedb.org/3/genre/movie/list?api_key=${API_KEY}&language=${lng}`;
+  `https://api.themoviedb.org/3/genre/movie/list?api_key=${process.env.REACT_APP_API_KEY}&language=${lng}`;
+
+//movies
+export const getMoviesUrl = (lng: string, genre: string) =>
+  `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_API_KEY}&language=${lng}&sort_by=popularity.desc&include_adult=false&include_video=false&with_genres=${genre}&with_watch_monetization_types=flatrate`;
