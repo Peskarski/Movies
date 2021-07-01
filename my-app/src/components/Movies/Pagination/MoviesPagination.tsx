b3d65dd583a9e59d2388d1821b398f605bd089dc
Peskarsky <v.peskarskij@gmail.com>
2021-07-01 18:54:17 +0300

    added page reset after filters applying

--- my-app/src/components/Movies/Pagination/MoviesPagination.tsx
+++ my-app/src/components/Movies/Pagination/MoviesPagination.tsx
@@ -5,13 +5,20 @@ import { StyledPagination, StyledContainer } from './styles';
 
 interface Pagination {
   changePage: (page: number) => void;
+  page: number;
 }
 
-export const MoviesPagination: React.FC<Pagination> = ({ changePage }) => {
+export const MoviesPagination: React.FC<Pagination> = ({ changePage, page }) => {
   const pages = useSelector(totalPages);
   return (
     <StyledContainer>
-      <StyledPagination count={pages} size="large" shape="rounded" onChange={(e, page) => changePage(page)} />
+      <StyledPagination
+        count={pages}
+        size="large"
+        shape="rounded"
+        onChange={(e, page) => changePage(page)}
+        page={page}
+      />
     </StyledContainer>
   );
 };
