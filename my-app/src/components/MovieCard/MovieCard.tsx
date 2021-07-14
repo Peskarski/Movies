dc72a1ee2f759d9a18c3f7091fb8c5db6aae3783
Peskarsky <v.peskarskij@gmail.com>
2021-07-14 15:32:09 +0300

    added popover to movie cards

--- my-app/src/components/MovieCard/MovieCard.tsx
+++ my-app/src/components/MovieCard/MovieCard.tsx
@@ -1,22 +1,56 @@
 import React from 'react';
 import { ListItemData } from '../Dashboard';
-import { CardContent, CardMedia, CardActionArea } from '@material-ui/core';
+import { CardContent, CardMedia, CardActionArea, Popper, IconButton } from '@material-ui/core';
 import { getPosterUrl } from '../../API';
 import { useHistory } from 'react-router-dom';
-import { StyledCard } from './styles';
+import { StyledCard, StyledPopperContainer, StyledPopper } from './styles';
+import { useSelector } from 'react-redux';
+import { sessionID } from '../LogIn/store';
+import AddIcon from '@material-ui/icons/Add';
 
 export const MovieCard: React.FC<ListItemData> = ({ title, id, poster_path }) => {
   const posterSrc = getPosterUrl(poster_path);
   const history = useHistory();
+  const session_id = useSelector(sessionID);
+
+  const [anchorEl, setAnchorEl] = React.useState<HTMLElement | null>(null);
+  const open = Boolean(anchorEl);
+
+  const openPopper = (e: React.MouseEvent<HTMLElement, MouseEvent>) => {
+    setAnchorEl(e.currentTarget);
+  };
+
+  const closePopper = () => {
+    setAnchorEl(null);
+  };
 
   return (
-    <StyledCard onClick={() => history.push(`/movie-details/${id}`)}>
-      <CardActionArea>
-        <CardMedia component="img" alt="Poster" image={posterSrc} />
-        <CardContent>
-          <h4>{title}</h4>
-        </CardContent>
-      </CardActionArea>
-    </StyledCard>
+    <>
+      <StyledCard onMouseEnter={openPopper} onMouseLeave={closePopper}>
+        <CardActionArea
+          onClick={(e) => {
+            history.push(`/movie-details/${id}`);
+          }}
+        >
+          {session_id && (
+            <StyledPopper open={open} anchorEl={anchorEl} placement="top">
+              <StyledPopperContainer
+                onClick={(e) => {
+                  e.stopPropagation();
+                }}
+              >
+                <IconButton color="secondary" onClick={() => console.log(id)}>
+                  <AddIcon />
+                </IconButton>
+              </StyledPopperContainer>
+            </StyledPopper>
+          )}
+          <CardMedia component="img" alt="Poster" image={posterSrc} />
+          <CardContent>
+            <h4>{title}</h4>
+          </CardContent>
+        </CardActionArea>
+      </StyledCard>
+    </>
   );
 };
--- my-app/src/components/MovieCard/styles.js
+++ my-app/src/components/MovieCard/styles.js
@@ -1,4 +1,4 @@
-import Card from '@material-ui/core/Card';
+import { Card, Container, Popper } from '@material-ui/core';
 import { styled } from '@material-ui/core/styles';
 
 export const StyledCard = styled(Card)({
@@ -6,10 +6,24 @@ export const StyledCard = styled(Card)({
   maxHeight: '380px',
   margin: ' 20px 40px',
   '& :hover': {
-    backgroundColor: 'rgb(107, 100, 100);',
+    backgroundColor: 'rgb(107, 100, 100)',
     color: 'white',
   },
   '& img': {
     maxHeight: '300px',
   },
+});
+
+export const StyledPopperContainer = styled(Container)({
+  width: '200px',
+  height: '30px',
+  display: 'flex',
+  justifyContent: 'space-around',
+  background: 'rgba(10, 10, 10, 0.2)',
+  borderRadius: '4px',
+});
+
+export const StyledPopper = styled(Popper)({
+  position: 'relative',
+  top: '30px !important',
 });
